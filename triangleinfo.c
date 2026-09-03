6797c9e04d51305eb48730785e0e3d9aca8e2e47
Bash0121 <Robert.smith@students.fhu.edu>
2026-09-03 15:29:47 -0500

    Added the triangle stuff

--- triangleinfo.c
+++ triangleinfo.c
@@ -1,54 +1,47 @@
 #include <stdio.h>
 #include <math.h>
 
-// build command 
-//gcc triangleinfo.c -o bin/triangleinfo
-
-double area(double a, double b, double c );
+double area(double a, double b, double c);
 
 int main(void) {
+    
     double a,b,c;
     printf("Enter side 1: ");
     scanf("%lf", &a);
+    // printf("%lf\n", a);
+
     printf("Enter side 2: ");
     scanf("%lf", &b);
+    // printf("%lf\n", b);
+
     printf("Enter side 3: ");
     scanf("%lf", &c);
+    // printf("%lf\n", c);
 
-    //printf("%lf\n", a);
-
-    // check if triangle is impossible, one size is >= than the other 2 added together
-    if ( (a >= b+c) || (b >= a+c) || (c> a+b) ) {
-        printf("impossible\n");
+    if (a >= b+c || b >= a+c || c> a+b) {
+        printf("Impossible\n");
         return 0;
     }
 
-    int ab_equal = (a==b);
-    int bc_equal = (b==c);
-    int ac_equal = (a==c);
-
-    if(ab_equal && bc_equal) {
-        // 
-        printf("equilateral\n");
-        printf("isosceles\n");
+    if (a==b&&b==c&&c==a){
+        printf("Equilateral\n");
+        printf("Isosceles\n");
     }
-    else if (ab_equal || bc_equal || ac_equal) {
-        printf("isosceles\n");
+    else if (a==b||b==c||c==a) {
+        printf("Isosceles\n");
     }
     else {
-        printf("scalene\n");
+        printf("Scalene\n");
     }
 
-    printf("area: %.2lf\n", area(a,b,c) );
+    printf("area: %.2lf\n", area(a,b,c));
 
     return 0;
 }
 
-// Heron's formula
-double area(double a, double b, double c ) {
-    // sqrt( s * (s-a) * (s-b) * (s-c) ), s = semiperimeter
+double area(double a, double b, double c) {
     double s = (a+b+c)/2.0;
-    double area = sqrt( s * (s-a) * (s-b) * (s-c) );
-
+    double area = sqrt(s * (s-a) * (s-b) * (s-c));
+    
     return area;
 }
